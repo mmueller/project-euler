1ef54f6a50344e302f41b8fca95d8adf5789f0a7
Mike Mueller <mike@subfocal.net>
2012-01-09 15:23:31 -0800

    Solve problem 34.

---
+++ p34.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+"""
+
+from math import factorial
+from p33 import get_digits
+
+def find_curious_numbers():
+    # Numbers obeying this rule cannot be larger than 7 digits, because
+    # 9! * 8 is 2903040, a 7-digit number.  (An 8 digit number can never
+    # produce itself in this way.)
+    limit = factorial(9) * 7 + 1
+    is_curious = lambda n: sum(map(factorial, get_digits(n))) == n
+    return filter(is_curious, range(10, limit))
+
+if __name__ == '__main__':
+    numbers = find_curious_numbers()
+    print 'Curious numbers:'
+    for n in numbers:
+        print ' ', n
+    print 'Sum:', sum(numbers)
