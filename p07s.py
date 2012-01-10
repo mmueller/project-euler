b30732ec63cace9b91306dd7a7d0161d4f3ca6a2
Mike Mueller <mike@subfocal.net>
2012-01-09 21:52:14 -0800

    Use new sieve in p07s.
    p07s had a basic sieve implementation, but using the generator from the new
    primes module allows me to stop at exactly the right point.  (Plus the new
    sieve implementation is much faster.)
    
    Runtime is now 0.077s instead of 1.455s.  I won't bother updating p07n, but it
    runs in about 18 seconds.

--- p07s.py
+++ p07s.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python
 
-def sieve(n):
-    """Return a list of primes less than n."""
-    if n < 2:
-        return []
+from primes import prime_generator
 
-    # Space is a list of tuples (bool, int) where the bool is true
-    # if the int might still be prime, false if we know it's not.
-    space = []
-    for i in range(0,n):
-        space.append((True, i))
-    primes = []
+# Old sieve implementation.  About 20 times slower for the same result.
+#def sieve(n):
+#    """Return a list of primes less than n."""
+#    if n < 2:
+#        return []
+#
+#    # Space is a list of tuples (bool, int) where the bool is true
+#    # if the int might still be prime, false if we know it's not.
+#    space = []
+#    for i in range(0,n):
+#        space.append((True, i))
+#    primes = []
+#
+#    for i in range(2,n):
+#        if space[i][0] == True:
+#            primes.append(space[i][1])
+#            for j in range(i*i,n,i):
+#                if space[j][0] == True and space[j][1] % space[i][1] == 0:
+#                    space[j] = (False, space[j][1])
+#
+#    return primes
 
-    for i in range(2,n):
-        if space[i][0] == True:
-            primes.append(space[i][1])
-            for j in range(i*i,n,i):
-                if space[j][0] == True and space[j][1] % space[i][1] == 0:
-                    space[j] = (False, space[j][1])
-
-    return primes
-
-# Assume the 10001st prime occurs below 100,000
 if __name__ == '__main__':
-    primes = sieve(1000000)
-    print primes[10000]
+    for i, p in enumerate(prime_generator()):
+        if i == 10000:
+            print p
+            break
