070f1895f0cad7ccc25de88061532ae98d205499
Mike Mueller <mike@subfocal.net>
2012-01-10 16:08:46 -0800

    Optimize p34 a bit.
    Runtime drops from 14 to 8 seconds.

--- p34.py
+++ p34.py
@@ -3,7 +3,8 @@
 """
 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
 
-Find the sum of all numbers which are equal to the sum of the factorial of their digits.
+Find the sum of all numbers which are equal to the sum of the factorial of
+their digits.
 
 Note: as 1! = 1 and 2! = 2 are not sums they are not included.
 """
@@ -12,12 +13,15 @@ from digits import get_digits
 from math import factorial
 
 def find_curious_numbers():
+    # Precompute factorials
+    facts = [factorial(x) for x in range(0, 10)]
+
     # Numbers obeying this rule cannot be larger than 7 digits, because
     # 9! * 8 is 2903040, a 7-digit number.  (An 8 digit number can never
     # produce itself in this way.)
-    limit = factorial(9) * 7 + 1
-    is_curious = lambda n: sum(map(factorial, get_digits(n))) == n
-    return filter(is_curious, range(10, limit))
+    limit = facts[9] * 7 + 1
+    is_curious = lambda n: sum([facts[x] for x in get_digits(n)]) == n
+    return filter(is_curious, xrange(10, limit))
 
 if __name__ == '__main__':
     numbers = find_curious_numbers()
